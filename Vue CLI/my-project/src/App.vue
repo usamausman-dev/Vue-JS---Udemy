5da88e31568b6e14bcc5344fbb60d502c7584980
Usama Usman <uusman004@gmail.com>
2022-08-05 01:16:08 +0500

    pushing files

--- Vue CLI/my-project/src/App.vue
+++ Vue CLI/my-project/src/App.vue
@@ -2,7 +2,22 @@
   <section>
     <h2>My friends</h2>
     <ul>
-      <friend-list name="Hamza" phone-number="03102257756"></friend-list>
+      <friend-list
+        v-for="friend in friends"
+        :key="friend.id"
+        :id="friend.id"
+        :name="friend.name"
+        :phone-number="friend.phone"
+        :is-favorite="friend.isFavorite"
+        @toggle-favorite="toggleFavStatus"
+      ></friend-list>
+
+      <!-- <friend-list
+        name="Usama"
+        phone-number="03040257471"
+        is-favorite="0"
+      ></friend-list> -->
+
       <!-- <friend-list></friend-list> -->
     </ul>
   </section>
@@ -17,18 +32,28 @@ export default {
           id: "076",
           name: "Osama",
           phone: "03102257759",
+          isFavorite: true,
         },
         {
           id: "059",
           name: "Hammad",
           phone: "03101234567",
+          isFavorite: false,
         },
       ],
     };
   },
-  methods: {},
+  methods: {
+    toggleFavStatus(friendId) {
+      const identifiedFriend = this.friends.find(
+        (friend) => friend.id === friendId
+      );
+      identifiedFriend.isFavorite = !identifiedFriend.isFavorite;
+    },
+  },
   computed: {},
 };
 </script>
+
 
 
--- Vue CLI/my-project/src/components/friendsList.vue
+++ Vue CLI/my-project/src/components/friendsList.vue
@@ -1,13 +1,18 @@
 <template>
   <li class="pt-5">
-    <h2>Name : {{ name }}</h2>
+    <h2>Name : {{ name }} {{ isFavorite ? "(Fav)" : "" }}</h2>
     <button @click="toggleDetails" class="btn btn-primary">
       {{ detailsVisible ? "Hide" : "Show" }} Details
     </button>
+
+    <button @click="toggleFav" class="btn btn-primary mx-2">
+      Toggle Favorite
+    </button>
+
     <div class="container my-5">
       <ul class="list-group" v-if="detailsVisible">
         <li class="list-group-item">{{ phoneNumber }}</li>
-        <li class="list-group-item">{{ friend.phone }}</li>
+        <!-- <li class="list-group-item">{{ friend.phone }}</li> -->
       </ul>
     </div>
   </li>
@@ -15,21 +20,49 @@
 
 <script>
 export default {
-  props: ["name", "phoneNumber"],
+  // props: ["name", "phoneNumber", "isFavorite"],
+
+  props: {
+    id: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    isFavorite: {
+      type: Boolean,
+      required: true,
+      default: false,
+      // validator : function (value) {
+      //   return value === '1' || value === '0'
+      // }
+    },
+  },
+
   data() {
     return {
       detailsVisible: false,
-      friend: {
-        id: "076",
-        name: "Osama",
-        phone: "03102257759",
-      },
+      // friendIsFav: this.isFavorite,
     };
   },
 
   methods: {
     toggleDetails() {
       this.detailsVisible = !this.detailsVisible;
+    },
+
+    // Due to Uni directional flow we can't update the props in child component , for this we need to pass
+    // the prop value in data and then update the data
+
+    toggleFav() {
+      // this.friendIsFav = !this.friendIsFav;
+      this.$emit("toggle-favorite", this.id);
     },
   },
 };
